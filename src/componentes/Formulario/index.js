840f0baf56c34388ae0b954d6385c75f29246947
Lucas Garcia <lucasugarcia@gmail.com>
2023-07-14 16:55:09 -0300

    Finalização do código da segunda aula, trabalhando com props, children

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,34 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+
+const Formulario = () => {
+    const times = [
+        'Administrativo',
+        'Desenvolvimento',
+        'Manutenção',
+        'Processamento',
+        'TI'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log("Form submetido")
+    }
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>Criar Card</Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
